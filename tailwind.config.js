7daae5d0c771e258ec27b6486c0024f528c9ff79
burak2482 <burak2482@gmail.com>
2024-10-08 21:02:32 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        customNormal: ['MyCustomFont1', 'sans-serif'],
+        customSemiBold: ['MyCustomFont2', 'sans-serif'],
+        customItalic: ['MyCustomFont3', 'sans-serif'],
+        customExtraBold: ['MyCustomFont4', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        customYellow: 'rgb(244, 208, 78)', 
+        customGrey: 'rgb(107, 107, 107)', 
+        customBlack: 'rgb(18, 18, 18)',
       },
     },
   },
